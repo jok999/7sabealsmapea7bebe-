7e514e16d66e8b4701ecd00996d9b0d98e2e98e7
jok999 <42048536+jok999@users.noreply.github.com>
2018-10-22 21:44:52 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+}); 
+
+
+
+client.on('ready', async() => {
+var server = "471777682334547991"; // ايدي السررفر
+var channel = "471777682816761857";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**كودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودز **')
+    },305);
+})
+
+
+
+client.login(process.env.BOT_TOKEN);
